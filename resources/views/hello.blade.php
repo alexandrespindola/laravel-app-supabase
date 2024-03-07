718c0238e9118c9872b79cdc1dc1b5f423c7d7fe
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-07 11:05:52 +0100

    git init

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Hello World</div>
+
+                    <div class="card-body text-white text-3xl">
+                        <h1>Hey</h1>Hello, this is my first Laravel Breeze page!
+                        <h2 class="bg-orange-500">My second Tailwind website</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
